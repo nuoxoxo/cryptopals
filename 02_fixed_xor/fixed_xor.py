f2cc13e0da207ba5f80301b13bd48f1e8238f9a1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-04-21 20:46:17 +0200

    02/edit

--- 02_fixed_xor/fixed_xor.py
+++ 02_fixed_xor/fixed_xor.py
@@ -1,16 +1,21 @@
 hex1 = '1c0111001f010100061a024b53535009181c'
+
 hex2 = '686974207468652062756c6c277320657965'
 
 def hex_str_to_int_list(s: str):
     return [ int(s[i:i + 2], 16) for i in range(0, len(s), 2) ]
 
-L, R = hex_str_to_int_list( hex1 ), hex_str_to_int_list( hex2 )
-xor = [hex(l ^ r)[2:]  for l, r in zip(L, R)]
+L = hex_str_to_int_list( hex1 )
+R = hex_str_to_int_list( hex2 )
 
-res = ''.join(xor)
-res2 = ''.join([chr(int(l ^ r))  for l, r in zip(L, R)])
+xor_str_list = [hex(l ^ r)[2:] for l, r in zip(L, R)]
+xor_chr_list = [chr(int(l ^ r)) for l, r in zip(L, R)]
 
-print('/res', res)
-print('/hidden -', res2)
+res = ''.join(xor_str_list)
+msg = ''.join(xor_chr_list)
+
+print('/result -', res)
+print('/hidden -', msg)
 
 assert res == '746865206b696420646f6e277420706c6179'
+
